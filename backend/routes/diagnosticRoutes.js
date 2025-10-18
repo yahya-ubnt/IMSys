61fba0369bc2161ff72d2c26d5410a0ab2ce2177
yahya-ubnt <hassanyahya254@gmail.com>
2025-10-18 19:58:58 +0300

    adminissuperadmindone

--- backend/routes/diagnosticRoutes.js
+++ backend/routes/diagnosticRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { param } = require('express-validator');
 const { runDiagnostic, getDiagnosticHistory, getDiagnosticLogById } = require('../controllers/diagnosticController.js');
-const { protect, isAdminTenant } = require('../middlewares/authMiddleware.js');
+const { protect, isSuperAdminOrAdminTenant } = require('../middlewares/authMiddleware.js');
 
 const userIdValidation = [param('userId', 'User ID is required and must be a valid Mongo ID').isMongoId()];
 const logIdValidation = [param('logId', 'Log ID is required and must be a valid Mongo ID').isMongoId()];
 
 router.route('/')
-  .post([protect, isAdminTenant], userIdValidation, runDiagnostic)
-  .get([protect, isAdminTenant], userIdValidation, getDiagnosticHistory);
+  .post([protect, isSuperAdminOrAdminTenant], userIdValidation, runDiagnostic)
+  .get([protect, isSuperAdminOrAdminTenant], userIdValidation, getDiagnosticHistory);
 
 router.route('/:logId')
-    .get([protect, isAdminTenant], userIdValidation, logIdValidation, getDiagnosticLogById);
+    .get([protect, isSuperAdminOrAdminTenant], userIdValidation, logIdValidation, getDiagnosticLogById);
 
 module.exports = router;
