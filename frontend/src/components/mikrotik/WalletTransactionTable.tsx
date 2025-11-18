7e89a6fd81b3ac2180712c073a69e65c733f1782
yahya-ubnt <hassanyahya254@gmail.com>
2025-11-18 19:02:00 +0300

    leadsdonebordersremoved

--- frontend/src/components/mikrotik/WalletTransactionTable.tsx
+++ frontend/src/components/mikrotik/WalletTransactionTable.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import {
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+} from "@tanstack/react-table";
 import { DataTable } from '@/components/data-table';
 import { columns, WalletTransaction } from '../../app/mikrotik/users/[id]/details/wallet-columns';
 
@@ -8,5 +15,14 @@ interface WalletTransactionTableProps {
 }
 
 export function WalletTransactionTable({ data }: WalletTransactionTableProps) {
-  return <DataTable columns={columns} data={data} />;
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+
+  return <DataTable table={table} columns={columns} />;
 }
